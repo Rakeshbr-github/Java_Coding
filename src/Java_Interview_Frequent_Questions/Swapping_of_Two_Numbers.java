7a240383ed031eff2820df63777a90fd4d75a887
Rakeshbr-github <rakeshgowda292000@gmail.com>
2024-12-26 10:18:19 +0530

    second commit

--- src/Java_Interview_Frequent_Questions/Swapping_of_Two_Numbers.java
+++ src/Java_Interview_Frequent_Questions/Swapping_of_Two_Numbers.java
@@ -3,8 +3,35 @@ package Java_Interview_Frequent_Questions;
 public class Swapping_of_Two_Numbers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int a=10,b=20;
+		
+//		Logic 1  - using third variable
+//		System.out.println("Before swapping the numbers value of a is " + a+ " and value of b is " +b);
+//		int t=a;
+//		a=b;
+//		b=t;
+//		System.out.println("After swapping the numbers value of a is " + a+ " and value of b is " +b);
+//		System.out.println();
+		
+//		Logic 2 - using +&- Without using third variable 
+//		a=a+b;
+//		b=a-b;
+//		a=a-b;
+//		System.out.println("After swapping a =" +a+ " and b=" +b );
+		
+//		Logic 3 - Use * and / without using third variable -in this a and b value must not be 0
+//		a=a*b;
+//		b=a/b;
+//		a=a/b;
+//		System.out.println("After swapping a =" +a+ " and b=" +b);
+		
+		
+//		Logic 4- Single statement
+		b=a+b-(a=b);
+		System.out.println("After swapping a=" +a+ " and b="+b);
+
+		
 	}
 
 }
