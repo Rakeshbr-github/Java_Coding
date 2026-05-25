f529f27fc72e7a1b8190c7c5d6004b24cb9610a3
Rakesh_RestAPI <rakeshh292000@gmail.com>
2026-05-25 19:29:26 +0530

    May 25th commit

--- src/String_Java_Programs/RemoveDuplicate_CharinString.java
+++ src/String_Java_Programs/RemoveDuplicate_CharinString.java
@@ -25,3 +25,27 @@ public class RemoveDuplicate_CharinString {
         return setofStrings.toString();
     }
 }
+
+////or as below
+//
+//class Main {
+//    public static void main(String[] args) {
+//        String s="Rakesh is a Automation tester";
+//        String s1=s.toLowerCase();
+//        char[] c=s1.toCharArray();
+//        Map<Character,Integer> duplicates=new HashMap<>();
+//        for(char ch:c)
+//        {
+//            duplicates.put(ch, duplicates.getOrDefault(ch,0)+1);
+//        }
+//        for(Map.Entry<Character,Integer> entry:duplicates.entrySet())
+//        {
+//            if(entry.getValue()<2)
+//            {
+//                System.out.print(entry.getKey());
+//            }
+//
+//        }
+//
+//
+//    }
