cf473955ff28c5eb4f6c53842c0d3a792900961c
Selenium_Prep <rakeshh292000@gmail.com>
2026-06-24 15:36:26 +0530

    June 24th commit

---
+++ src/Array_related/Find_LargestSumOf_ConsecutiveNuminArray.java
@@ -0,0 +1,27 @@
+package Array_related;
+
+public class Find_LargestSumOf_ConsecutiveNuminArray {
+
+    public static void main(String[] args)
+    {
+        int[] a={4,5,6,2,9,5};
+
+        int greatestsum=0;
+        int index1=0;
+        int index2=1;
+        for(int i=0;i<a.length-1;i++)
+        {
+            int num=a[i]+a[i+1];
+            if(num>greatestsum)
+            {
+                greatestsum=num;
+                 index1 = i;
+                 index2 = i + 1;
+            }
+
+        }
+        System.out.println("Maximum Sum: " + greatestsum);
+        System.out.println("Indices: " + index1 + ", " + index2);
+    }
+    }
+
--- src/Java_Interview_Frequent_Questions/Fibonnaci_series.java
+++ src/Java_Interview_Frequent_Questions/Fibonnaci_series.java
@@ -9,32 +9,32 @@ public class Fibonnaci_series {
 
 		
 //		logic 1
-//		int n1=0,n2=1,sum=0;
-//	      System.out.print(n1 + " " + n2 );
-//		for(int i=2;i<10;i++)
-//		{
-//			sum=n1+n2;
-//			System.out.print(" " + sum);
-//			n1=n2;
-//			n2=sum;
-//		}
+		int n1=0,n2=1,sum=0;
+	      System.out.print(n1 + " " + n2 );
+		for(int i=2;i<10;i++)
+		{
+			sum=n1+n2;
+			System.out.print(" " + sum);
+			n1=n2;
+			n2=sum;
+		}
 		
 		
 		
-//		logic 2 using arraylist conecpt
-		ArrayList<Integer> arr = new ArrayList<>();  //<> is optional on the right hand side 
-        for (int i = 0; i <= 10; i++) {
-            if (i <= 1) {
-                arr.add(i);       // arr[0,1]
-            } else {
-                int sum = arr.get(i - 1) + arr.get(i - 2);  //
-                arr.add(sum);     // arr[0,1,1,2,3,5,8,13,21,34,55]
-            }
-        }
-        for (int num : arr) 
-        {
-            System.out.print(num + " ");
-        }
+////		logic 2 using arraylist conecpt
+//		ArrayList<Integer> arr = new ArrayList<>();  //<> is optional on the right hand side
+//        for (int i = 0; i <= 10; i++) {
+//            if (i <= 1) {
+//                arr.add(i);       // arr[0,1]
+//            } else {
+//                int sum = arr.get(i - 1) + arr.get(i - 2);  //
+//                arr.add(sum);     // arr[0,1,1,2,3,5,8,13,21,34,55]
+//            }
+//        }
+//        for (int num : arr)
+//        {
+//            System.out.print(num + " ");
+//        }
 		
         
           
