6a519196f245f74cce5b8c6de7f5256c9d6bb07e
Selenium_Prep <rakeshh292000@gmail.com>
2026-06-25 16:21:20 +0530

    June 25th commit

--- src/Java_Interview_Frequent_Questions/Duplicates_in_A_Array.java
+++ src/Java_Interview_Frequent_Questions/Duplicates_in_A_Array.java
@@ -4,21 +4,54 @@ public class Duplicates_in_A_Array {
 
 	public static void main(String[] args) {
 
-		int a[]= {1,2,5,4,5,6};
+		int[] a = {1,2,3,4,2,2,5,6,6};
 		int arrlen=a.length;
-		for(int i=0;i<arrlen;i++)
-		{
-			for(int j=i+1;j<arrlen;j++)
-			{
-				if(a[i]==a[j])
-				{
-					System.out.println(a[i] + " Duplicate value in the array");
+//		for printing all duplicates
+//		for(int i=0;i<arrlen;i++)
+//		{
+//			for(int j=i+1;j<arrlen;j++)
+//			{
+//				if(a[i]==a[j])
+//				{
+//					System.out.println(a[i] + " Duplicate value in the array");
+//				}
+//			}
+//		}
+
+
+//		for printing only once for duplicate elements
+		for (int i = 0; i < a.length; i++) {
+
+			boolean duplicateFound = false;
+			boolean alreadyPrinted = false;
+
+			// Check if already printed
+			for (int k = 0; k < i; k++) {
+				if (a[i] == a[k]) {
+					alreadyPrinted = true;
+					break;
 				}
 			}
-			
-			
-			
+
+			if (alreadyPrinted)
+				continue;
+
+			// Check duplicate ahead
+			for (int j = i + 1; j < a.length; j++) {
+				if (a[i] == a[j]) {
+					duplicateFound = true;
+					break;
+				}
+			}
+
+			if (duplicateFound) {
+				System.out.println(a[i]);
+			}
 		}
+
+
+
+
 		}
 	}
 
